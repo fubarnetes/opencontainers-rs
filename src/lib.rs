9de50f45e3630c53c19c4c1d863e9f761c09b53f
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2019-04-22 08:16:44 +0200

    added image struct

--- src/lib.rs
+++ src/lib.rs
@@ -52,6 +52,12 @@ pub struct Registry {
     credential_cache: TtlCache<String, Credential>,
 }
 
+impl std::fmt::Debug for Registry {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Registry {{ url: {}, client: {:?} }}", self.url, self.client)
+    }
+}
+
 impl Registry {
     /// Create a new registry interface given the URL to a registry.
     ///
@@ -200,5 +206,62 @@ impl Registry {
             .map_err(RegistryError::ReqwestError)?
             .parse()
             .map_err(RegistryError::ManifestError)
+    }
+
+    /// Create an image handle for a given image
+    ///
+    /// # Example
+    /// ```
+    ///# extern crate opencontainers;
+    ///# use opencontainers::Registry;
+    ///# let registry = Registry::new("https://registry-1.docker.io");
+    /// let manifest = registry.image("hello-world", "latest")
+    ///     .expect("Could not get image");
+    /// ```
+    pub fn image(&self, name: &str, reference: &str) -> Result<Image, RegistryError> {
+        Image::new(self, name, reference)
+    }
+}
+
+#[derive(Debug)]
+pub struct Image<'a> {
+    registry: &'a Registry,
+    manifest: ManifestV2,
+}
+
+impl<'a> Image<'a> {
+    /// Create a new image given a specific repository
+    ///
+    /// Consider using [Registry::image] instead.
+    ///
+    /// # Example
+    /// ```
+    ///# extern crate opencontainers;
+    ///# use opencontainers::Registry;
+    ///# let registry = Registry::new("https://registry-1.docker.io");
+    /// let image = opencontainers::Image::new(&registry, "hello-world", "latest")
+    ///     .expect("Could not get image");
+    /// ```
+    pub fn new(registry: &'a Registry, name: &str, reference: &str) -> Result<Self, RegistryError> {
+        let manifest = registry.manifest(name, reference)?;
+        Ok(Self {
+            registry,
+            manifest,
+        })
+    }
+
+    /// Return an image manifest
+    ///
+    /// # Example
+    /// ```
+    ///# extern crate opencontainers;
+    ///# use opencontainers::Registry;
+    ///# let registry = Registry::new("https://registry-1.docker.io");
+    /// let manifest = registry.image("hello-world", "latest")
+    ///     .expect("Could not get image")
+    ///     .manifest();
+    /// ```
+    pub fn manifest(&self) -> &ManifestV2 {
+        &self.manifest
     }
 }
