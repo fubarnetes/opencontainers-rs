cab9cec06f3aaa2ba7252e07596756464ddbce0d
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2019-08-10 15:57:28 +0200

    Unpack trait: refactor and add docs

--- src/glue.rs
+++ src/glue.rs
@@ -86,13 +86,27 @@ fn get_whiteout_path_windows<P: AsRef<std::path::Path>>(path: P) -> Option<std::
     path.set_file_name(std::ffi::OsString::from_wide(&wide[4..]));
     Some(path)
 }
-/// A trait that describes the actions required to create a container's root filesystem
-/// from an image.
+/// A trait that describes the actions required to create a container's root
+/// filesystem from an image.
+///
+/// # Safety
+/// To ensure safe handling of the extraction, implementations of [Unpack::add],
+/// [Unpack::whiteout_file] and [Unpack::whiteout_folder] *MUST* ensure that
+/// these actions apply only to files within the root filesystem bundle.
+///
+/// While this can be achieved using [tar::Entry::unpack_in] when implementing
+/// [Unpack::add], the whiteout handlers *MUST* take care of this individually.
+///
+/// Implementations that fail to address this are likely to be vulnerable to
+/// file system traversal vulnerabilities due to the possibility of `..` being
+/// present in the paths contained in the tarball.
 pub trait Unpack {
-    /// Called in order for each layer in the image
-    fn apply<R: std::io::Read>(&self, layer: tar::Archive<R>) -> Result<(), UnpackError>;
-    fn apply_change<R: std::io::Read>(&self, entry: tar::Entry<R>) -> Result<(), UnpackError>;
-
+    /// The main entrypoint for unpacking an image
+    ///
+    /// This will call [Unpack::apply_layer] for each layer in the image, which
+    /// will in turn dispatch the [Unpack::add], [Unpack::whiteout_file] and
+    /// [Unpack::whiteout_folder] trait methods for each file or whiteout,
+    /// respectively.
     fn unpack(&self, image: &Image) -> Result<(), crate::Error> {
         for layer in image
             .manifest()
@@ -102,9 +116,105 @@ pub trait Unpack {
             let tar = image
                 .get_layer(layer)
                 .map_err(crate::Error::RegistryError)?;
-            self.apply(tar).map_err(crate::Error::UnpackError)?;
+            self.apply_layer(tar).map_err(crate::Error::UnpackError)?;
         }
 
+        Ok(())
+    }
+
+    /// Called in order for each layer in the image
+    ///
+    /// The default implementation calls the [Unpack::pre_apply] and
+    /// [Unpack::post_apply] hooks before and after calling
+    /// [Unpack::apply_change] for each entry in the changeset.
+    fn apply_layer<R: std::io::Read>(&self, mut layer: tar::Archive<R>) -> Result<(), UnpackError> {
+        self.pre_apply()?;
+
+        for entry in layer.entries().map_err(UnpackError::GetEntries)? {
+            let entry = entry.map_err(UnpackError::GetEntry)?;
+            self.apply_change(entry)?;
+        }
+
+        self.post_apply()
+    }
+
+    /// Applies a change contained in the tar archive
+    ///
+    /// Determines the type of change to apply, and dispatches [Unpack::add],
+    /// [Unpack::whiteout_file] and [Unpack::whiteout_folder] for this change.
+    fn apply_change<R: std::io::Read>(&self, entry: tar::Entry<R>) -> Result<(), UnpackError> {
+        let path: std::path::PathBuf = entry.path().map_err(UnpackError::GetEntryPath)?.into();
+        if let Some(filename) = path.file_name() {
+            if filename == std::ffi::OsStr::new(".wh..wh..opq") {
+                // FIXME: Removethe whiteout filename.
+                return self.whiteout_folder(path);
+            }
+
+            if let Some(whiteout_path) = get_whiteout_path(path) {
+                return self.whiteout_file(whiteout_path);
+            }
+        }
+
+        self.add(entry)
+    }
+
+    /// Implement this to handle additions and changes
+    ///
+    /// # Safety
+    /// See the Trait-level documentation for safe implementation notes.
+    ///
+    /// It is recommended to use [tar::Entry::unpack_in] for safe path handling.
+    ///
+    /// # Example
+    /// ```
+    ///# use opencontainers::glue::Unpack;
+    ///# use opencontainers::glue::UnpackError;
+    /// use std::path::PathBuf;
+    /// struct Extractor { root: PathBuf };
+    ///
+    /// impl Unpack for Extractor {
+    ///     fn add<R: std::io::Read>(&self, mut entry: tar::Entry<R>) -> Result<(), UnpackError> {
+    ///         let path: std::path::PathBuf = entry.path().map_err(UnpackError::GetEntryPath)?.into();
+    ///         if !entry
+    ///             .unpack_in(self.root.as_path())
+    ///             .map_err(UnpackError::UnpackEntry)?
+    ///         {
+    ///             return Err(UnpackError::AttemptedFilesystemTraversal(path));
+    ///         }
+
+    ///         Ok(())
+    ///     }
+    ///# fn whiteout_file<P: AsRef<std::path::Path>>(&self, path: P) -> Result<(), UnpackError> { Ok(()) }
+    ///# fn whiteout_folder<P: AsRef<std::path::Path>>(&self, path: P) -> Result<(), UnpackError> { Ok(()) }
+    /// }
+    /// ```
+    fn add<R: std::io::Read>(&self, entry: tar::Entry<R>) -> Result<(), UnpackError>;
+
+    /// Implement this to handle individual file whiteouts
+    ///
+    /// # Safety
+    /// See the Trait-level documentation for safe implementation notes.
+    fn whiteout_file<P: AsRef<std::path::Path>>(&self, path: P) -> Result<(), UnpackError>;
+
+    /// Implement this to whiteout a complete folder
+    ///
+    /// # Safety
+    /// See the Trait-level documentation for safe implementation notes.
+    fn whiteout_folder<P: AsRef<std::path::Path>>(&self, path: P) -> Result<(), UnpackError>;
+
+    /// Called before applying a layer
+    ///
+    /// Can be overriden to provide a callback before extraction of the files,
+    /// e.g. to create a snapshot or copy of the existing state.
+    fn pre_apply(&self) -> Result<(), UnpackError> {
+        Ok(())
+    }
+
+    /// Called after applying a layer
+    ///
+    /// Can be overriden to provide a callback after extraction of the files,
+    /// e.g. to create a snapshot or copy of the newly extracted state.
+    fn post_apply(&self) -> Result<(), UnpackError> {
         Ok(())
     }
 }
@@ -127,17 +237,8 @@ impl SimpleFolderUnpacker {
 }
 
 impl Unpack for SimpleFolderUnpacker {
-    fn apply_change<R: std::io::Read>(&self, mut entry: tar::Entry<R>) -> Result<(), UnpackError> {
+    fn add<R: std::io::Read>(&self, mut entry: tar::Entry<R>) -> Result<(), UnpackError> {
         let path: std::path::PathBuf = entry.path().map_err(UnpackError::GetEntryPath)?.into();
-        if let Some(filename) = path.file_name() {
-            if filename == std::ffi::OsStr::new(".wh..wh..opq") {
-                // TODO: Whiteout the complete directory including all siblings.
-                return Ok(());
-            }
-
-            if filename.to_string_lossy().starts_with(".wh.") {}
-        }
-
         // Apply addition or modification:
         // Additions and Modifications are represented the same in the changeset tar archive.
         if !entry
@@ -150,14 +251,15 @@ impl Unpack for SimpleFolderUnpacker {
         Ok(())
     }
 
-    fn apply<R: std::io::Read>(&self, mut layer: tar::Archive<R>) -> Result<(), UnpackError> {
-        let entries = layer.entries().map_err(UnpackError::GetEntries)?;
+    fn whiteout_file<P: AsRef<std::path::Path>>(&self, path: P) -> Result<(), UnpackError> {
+        // FIXME: implement whiteout_file.
+        println!("whiteout file: {}", path.as_ref().to_string_lossy());
+        Ok(())
+    }
 
-        for entry in entries {
-            let entry = entry.map_err(UnpackError::GetEntry)?;
-            self.apply_change(entry)?;
-        }
-
+    fn whiteout_folder<P: AsRef<std::path::Path>>(&self, path: P) -> Result<(), UnpackError> {
+        // FIXME: implement whiteout_folder.
+        println!("whiteout file: {}", path.as_ref().to_string_lossy());
         Ok(())
     }
 }
